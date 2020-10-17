d6da8764af17583f812af826caf8406aa1dc1cfe
rowsim <rowanq@live.co.uk>
2020-10-17 18:15:01 +0100

    Updates

---
+++ precache-manifest.cca67d6ab79d495c0c8a7f7f9bbabb1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6dd122eae32429e6294c5e7d975a23",
+    "url": "/spotify-favourites/index.html"
+  },
+  {
+    "revision": "cf8fccd85a33690a58e9",
+    "url": "/spotify-favourites/static/css/main.fb62864c.chunk.css"
+  },
+  {
+    "revision": "21c8e5799e924289eaf3",
+    "url": "/spotify-favourites/static/js/2.58a4f410.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/spotify-favourites/static/js/2.58a4f410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8fccd85a33690a58e9",
+    "url": "/spotify-favourites/static/js/main.b4ddbbb1.chunk.js"
+  },
+  {
+    "revision": "ccc2d887bdf84d1d0586",
+    "url": "/spotify-favourites/static/js/runtime-main.399bbb71.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/spotify-favourites/precache-manifest.cca67d6ab79d495c0c8a7f7f9bbabb1f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotify-favourites/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
