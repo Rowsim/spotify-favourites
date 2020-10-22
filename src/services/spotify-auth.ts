16bc8e0552a3053bcda4782c416df274d25890f1
rowsim <rowanq@live.co.uk>
2020-10-22 23:58:02 +0100

    adding react_app to env variable

--- src/services/spotify-auth.ts
+++ src/services/spotify-auth.ts
@@ -5,7 +5,7 @@ const sha256 = require("crypto-js/sha256");
 const Base64 = require("crypto-js/enc-base64");
 
 const REDIRECT_URI =
-  process.env.AUTH_REDIRECT_URI || "http://localhost:3000/callback";
+  process.env.REACT_APP_AUTH_REDIRECT_URI || "http://localhost:3000/callback";
 const CLIENT_ID = "a3c9a895bbe94102a08574ba4f4adc9e";
 const REQUEST_SCOPES = "user-top-read";
 
