fb057c560ad59f92ae92550747c52e0bc40b2c7d
rowsim <rowanq@live.co.uk>
2020-11-24 23:22:10 +0000

    adding fill animations to svg

--- src/components/svg-animations/StyledGirl.tsx
+++ src/components/svg-animations/StyledGirl.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import { ReactComponent as GirlMusicSvg } from "../../assets/images/girl-music.svg";
 import "./styled-girl.scss";
-export const StyledGirl = () => <GirlMusicSvg />;
+export const StyledGirl = () => <GirlMusicSvg className="fill-animation" />;
